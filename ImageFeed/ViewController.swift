3b54d782357e969521c638dccf92fe4f77fdd82a
wallterbrown <dev@nagaev-nagaev.ru>
2024-05-03 11:10:29 +0300

    Splash screen added. Color assets also

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    // Mark: меняем статус-бар на светлый
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
 }
 
