58a938408350998f819ec8ff019a0ee9b3c2a1b6
wallterbrown <dev@nagaev-nagaev.ru>
2024-05-14 13:21:30 +0300

    UITableView added. Preparing to finish work

---
+++ ImageFeed/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Всеволод Нагаев on 12.05.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+}
