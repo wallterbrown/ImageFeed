ac7e8635255d9dd805c5eaf995b709142d1a345e
wallterbrown <dev@nagaev-nagaev.ru>
2024-05-26 10:30:16 +0300

    Final result of 8th sprint

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,9 +5,12 @@
 //  Created by Всеволод Нагаев on 12.05.2024.
 //
 
-import Foundation
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    
+    static let reuseIdentifier = "ImagesListCell"
+
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
 }
