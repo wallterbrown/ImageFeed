46af4d65629ce899401f02ead945cf37febd6caf
wallterbrown <dev@nagaev-nagaev.ru>
2024-05-31 21:43:51 +0300

    IBOutlet && IBAction added

--- ProfileViewController.swift
+++ ProfileViewController.swift
@@ -5,6 +5,15 @@
 //  Created by Всеволод Нагаев on 29.05.2024.
 //
 import UIKit
-final class ProfileViewController: UIViewController{}
+final class ProfileViewController: UIViewController{
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var exitButton: UIButton!
+    @IBOutlet var userNameLable: UILabel!
+    @IBOutlet var loginNameLable: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBAction func didTapExitButton() {
+    }
+}
 
 
