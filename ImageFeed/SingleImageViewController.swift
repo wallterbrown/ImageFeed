bb45631b44b15b7ed9aea48c5c30fabc409ece04
wallterbrown <dev@nagaev-nagaev.ru>
2024-06-09 12:16:30 +0300

    Версия с сигвеем

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -7,5 +7,12 @@
 
 import UIKit
 final class SingleImageViewController: UIViewController{
-    @IBOutlet var imageView: UIImageView!
+    var image: UIImage?
+    
+    @IBOutlet var imageView: UIImageView! //private?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
