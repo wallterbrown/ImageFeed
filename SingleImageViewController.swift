e87d095961f96741895fcec4861d99308e270089
wallterbrown <dev@nagaev-nagaev.ru>
2024-06-09 10:37:23 +0300

    Фиксирую промежуточные изменения в проекте

---
+++ SingleImageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Всеволод Нагаев on 09.06.2024.
+//
+
+import UIKit
+final class SingleImageViewController: UIViewController{
+    @IBOutlet var imageView: UIImageView!
+}
